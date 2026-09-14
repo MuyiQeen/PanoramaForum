f6262b57b5b11cbb0e30c01de9145d14bcc3b0bc
CQ_YI <2517263761@qq.com>
2026-09-14 18:09:49 +0800

    new

--- backend/src/main/java/com/example/demo/constant/WebConstants.java
+++ backend/src/main/java/com/example/demo/constant/WebConstants.java
@@ -1,6 +1,9 @@
 package com.example.demo.constant;
 
+import com.example.demo.dto.response.Result;
+
 public final class WebConstants {
 
     public static final String HTTP_STATUS_ATTR = "httpStatus";
+
 }
