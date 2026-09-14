34ea5da2e0f9e2829697bfc80091e4ae21c46dc4
CQ_YI <2517263761@qq.com>
2026-09-15 00:24:31 +0800

    entity user,board

---
+++ frontend/src/modules/admin/stores/adminStore.js
@@ -0,0 +1,24 @@
+import { error } from "node:console";
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+
+export const adminStore = defineStore('adminStore', ()=>{
+    const access_token = ref('')
+    const user = ref('')
+    const isAuthenticated = ref('')
+    const loading = ref(false)
+    const islogged = computed(()=>!access_token.value && !user.value)
+
+    /**
+     * admin login
+     * @param {string} email 
+     * @param {string} password 
+     * @returns 
+     */
+    const login = async(email, password)=>{
+        if(!email.trim() || !password.trim())return ''
+
+        await fetch('')
+    }
+
+})
